2c95d63e2494a59f912f6d386c0df1597c942397
a23vinge <a23vinge@student.his.se>
2024-04-15 15:58:00 +0200

    adderade en till import och bundle=getIntent

--- app/src/main/java/com/example/screens/MainActivity2.java
+++ app/src/main/java/com/example/screens/MainActivity2.java
@@ -3,6 +3,7 @@ package com.example.screens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +11,15 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String name = extras.getString("name");
+            int number = extras.getInt("number");
+            // Do something with the name and number
+
+            TextView text1 = findViewById(R.id.text2);
+            text1.setText(name.concat(Integer.toString(number)));
+        }
     }
 }
